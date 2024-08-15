f2427ec35165c5ab314c96ee91126ceeeab6e625
ckaap <94190766+ckaap@users.noreply.github.com>
2024-08-15 12:43:32 +0300

    Add files via upload

---
+++ 1_Аудит AD.ps1
@@ -0,0 +1,251 @@
+﻿<#
+Скрипт предназначен для сбора и экспорта данных об объектах Active Directory, включая количество компьютеров, рабочих станций, серверов, пользователей и групп, а также активных объектов. 
+Он создает отчет с подробной информацией, записывая результаты в текстовый файл.
+Скрипт автоматически определяет путь сохранения данных и предоставляет удобочитаемые результаты, что облегчает анализ состояния инфраструктуры. 
+#>
+
+
+$Computers = (Get-ADComputer -Filter * | Measure-Object).Count
+$Workstation = (Get-ADComputer -Filter { OperatingSystem -notlike "*Server*"} | Measure-Object).Count
+$Servers = (Get-ADComputer -Filter { OperatingSystem -like "*Server*"} | Measure-Object).Count
+$Users = (Get-ADUser -Filter * | Measure-Object).Count
+$Groups = (Get-ADGroup -Filter * | Measure-Object).Count
+
+
+$mainPath ="$env:USERPROFILE\Desktop\" 
+$mainCount =$mainPath+"mainCount.txt"
+
+$ComputersActive = (Get-ADComputer -Filter {Enabled -eq $true} | Measure-Object).Count
+$WorkstationActive = (Get-ADComputer -Filter { OperatingSystem -notlike "*Server*" -and Enabled -eq $true } | Measure-Object).Count
+$ServersActive = (Get-ADComputer -Filter { OperatingSystem -like "*Server*" -and Enabled -eq $true} | Measure-Object).Count
+$UsersActive = (Get-ADUser -Filter {Enabled -eq $true} | Measure-Object).Count
+$GroupsActive = (Get-ADGroup -Filter * | Measure-Object).Count
+
+Write-Host "-----------------------Общее количество------------------------------"
+"-----------------------Общее количество------------------------------"| Out-File -FilePath $mainCount -Append
+Write-Host "Computers = $Computers" 
+"Computers = "+$Computers| Out-File -FilePath $mainCount -Append
+Write-Host "Workstation = $Workstation"
+"Workstation = "+$Workstation| Out-File -FilePath $mainCount -Append
+Write-Host "Servers = $Servers"
+"Servers = "+$Servers| Out-File -FilePath $mainCount -Append
+Write-Host "Users = $Users"
+"Users = "+$Users| Out-File -FilePath $mainCount -Append
+Write-Host "Groups = $Groups"
+"Groups = "+$Groups| Out-File -FilePath $mainCount -Append
+Write-Host "-----------------------------------------------------------------------"
+
+Write-Host "-----------------------Количество активных------------------------------"
+"-----------------------Количество активных------------------------------"| Out-File -FilePath $mainCount -Append
+Write-Host "ComputersActive = $ComputersActive"
+"ComputersActive = "+$ComputersActive| Out-File -FilePath $mainCount -Append
+Write-Host "WorkstationActive = $WorkstationActive"
+"WorkstationActive = "+$WorkstationActive| Out-File -FilePath $mainCount -Append
+Write-Host "ServersActive = $ServersActive"
+"ServersActive = "+$ServersActive| Out-File -FilePath $mainCount -Append
+Write-Host "UsersActive = $UsersActive"
+"UsersActive = "+$UsersActive| Out-File -FilePath $mainCount -Append
+Write-Host "GroupsActive = $GroupsActive"
+"GroupsActive = "+$GroupsActive| Out-File -FilePath $mainCount -Append
+Write-Host "-----------------------------------------------------------------------"
+
+
+$ADForest = (Get-ADDomain).Forest
+$ADForestMode = (Get-ADForest).ForestMode
+$ADDomainMode = (Get-ADDomain).DomainMode
+
+
+Write-Host "---------------------Информация о домене и лесе---------------------------------"
+"---------------------Информация о домене и лесе---------------------------------"| Out-File -FilePath $mainCount -Append
+Write-Host "Active Directory Forest Name = $ADForest"
+"Active Directory Forest Name="+$ADForest| Out-File -FilePath $mainCount -Append
+Write-Host "Active Directory Forest Mode = $ADForestMode"
+"Active Directory Forest Mode="+$ADForestMode| Out-File -FilePath $mainCount -Append
+Write-Host "Active Directory Domain Mode = $ADDomainMode"
+"Active Directory Domain Mode="+$ADDomainMode| Out-File -FilePath $mainCount -Append
+
+Write-Host "-----------------------------------------------------------------------"
+
+$ADVer = Get-ADObject (Get-ADRootDSE).schemaNamingContext -property objectVersion | Select-Object objectVersion
+$ADNum = $ADVer -replace "@{objectVersion=","" -replace "}",""
+If ($ADNum -eq '88') {$srv = 'Windows Server 2019/Windows Server 2022'}
+ElseIf ($ADNum -eq '87') {$srv = 'Windows Server 2016'}
+ElseIf ($ADNum -eq '69') {$srv = 'Windows Server 2012 R2'}
+ElseIf ($ADNum -eq '56') {$srv = 'Windows Server 2012'}
+ElseIf ($ADNum -eq '47') {$srv = 'Windows Server 2008 R2'}
+ElseIf ($ADNum -eq '44') {$srv = 'Windows Server 2008'}
+ElseIf ($ADNum -eq '31') {$srv = 'Windows Server 2003 R2'}
+ElseIf ($ADNum -eq '30') {$srv = 'Windows Server 2003'}
+
+
+#FSMO
+$Forest = Get-ADForest
+$SchemaMaster = $Forest.SchemaMaster
+$DomainNamingMaster = $Forest.DomainNamingMaster
+$Domain = Get-ADDomain
+$RIDMaster = $Domain.RIDMaster
+$PDCEmulator = $Domain.PDCEmulator
+$InfrastructureMaster = $Domain.InfrastructureMaster
+
+Write-Host "----------------------Информация о FSMO ролях--------------------------------"
+"----------------------Информация о FSMO ролях--------------------------------"| Out-File -FilePath $mainCount -Append
+Write-Host "SchemaMaster = $SchemaMaster"
+"SchemaMaster="+$SchemaMaster| Out-File -FilePath $mainCount -Append
+Write-Host "Domain Naming Master = $DomainNamingMaster"
+"Domain Naming Master="+$DomainNamingMaster| Out-File -FilePath $mainCount -Append
+Write-Host "RID Master = $RIDMaster"
+"RID Master="+$RIDMaster| Out-File -FilePath $mainCount -Append
+Write-Host "PDCEmulator = $PDCEmulator"
+"PDCEmulator="+$PDCEmulator| Out-File -FilePath $mainCount -Append
+Write-Host "Infrastructure Master = $InfrastructureMaster"
+"Infrastructure Master="+$InfrastructureMaster| Out-File -FilePath $mainCount -Append
+Write-Host "-----------------------------------------------------------------------"
+
+$exportPathPE =$mainPath+"PasswordNotExpires.txt" 
+#Проверяем пользователей у которых пароль не истекает
+$PasswordNotExpires = Get-ADuser -Filter {PasswordNeverExpires -eq $true -and Enabled -eq $true} -Properties Name,Enabled,DisplayName,PasswordNeverExpires,whenCreated
+$PasswordNotExpires | Export-Csv -Path $exportPathPE -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+$exportPathP9l=$mainPath+"Password90Left.txt"
+#Проверяем пользователей у которых пароль не менялся 90 дней
+$DateThrehold = (Get-Date).AddDays(-91)
+$Password90Left = Get-AdUser -Filter {PasswordLastSet -lt $DateThrehold -and Enabled -eq $true} -Properties Name,Enabled,DisplayName,PasswordNeverExpires,PasswordLastSet,whenCreated
+$Password90Left | Export-Csv -Path $exportPathP9l -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+#Пользователи, которые не входили 90 дней
+$exportPathL9l=$mainPath+"Logon90Left.txt"
+$DateThrehold = (Get-Date).AddDays(-91)
+$Logon90Left = Get-AdUser -Filter {LastLogonDate -lt $DateThrehold -and Enabled -eq $true} -Properties Name,Enabled,DisplayName,PasswordNeverExpires,PasswordLastSet,whenCreated
+$Logon90Left | Export-Csv -Path $exportPathL9l -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+#Получаем группы по типу
+
+$GroupSecurity = (Get-ADGroup -Filter {GroupCategory -eq "Security"} | Measure-Object).Count
+$GroupSecurityGlobal = (Get-ADGroup -Filter {GroupCategory -eq "Security" -and GroupScope -eq "Global"} | Measure-Object).Count
+$GroupSecurityDomainLocal = (Get-ADGroup -Filter {GroupCategory -eq "Security" -and GroupScope -eq "DomainLocal"} | Measure-Object).Count
+$GroupSecurityUniversal = (Get-ADGroup -Filter {GroupCategory -eq "Security" -and GroupScope -eq "Universal"} | Measure-Object).Count
+$GroupDistribution = (Get-ADGroup -Filter {GroupCategory -eq "Distribution"} | Measure-Object).Count
+
+Write-Host "----------------------Информация о группах--------------------------------"
+"----------------------Информация о группах--------------------------------"| Out-File -FilePath $mainCount -Append
+Write-Host "Групп Security = $GroupSecurity"
+"Групп Security="+$GroupSecurity| Out-File -FilePath $mainCount -Append
+Write-Host "Групп Security Global = $GroupSecurityGlobal"
+"Групп Security Global="+$GroupSecurityGlobal| Out-File -FilePath $mainCount -Append
+Write-Host "Групп Security DomainLocal = $GroupSecurityDomainLocal"
+"Групп Security DomainLocal="+$GroupSecurityDomainLocal| Out-File -FilePath $mainCount -Append
+Write-Host "Групп Security Universal = $GroupSecurityUniversal"
+"Групп Security Universal="+$GroupSecurityUniversal| Out-File -FilePath $mainCount -Append
+Write-Host "Групп Distribution = $GroupDistribution"
+"Групп Distribution="+$GroupDistribution| Out-File -FilePath $mainCount -Append
+Write-Host "-----------------------------------------------------------------------"
+
+#получаем группы в которых нет участников
+$exportPathNG =$mainPath+"nullGroup.txt"
+$nullGroup= Get-ADGroup -Filter * -Properties Members | where {-not $_.members} | Select-Object Name, DistinguishedName,description
+$nullGroup | Export-Csv -Path $exportPathNG -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+Write-Host "----------------------Группы в которых нет участников--------------------------------"
+$nullGroupCount = (Get-ADGroup -Filter * -Properties Members | where {-not $_.members} | Measure-Object).Count
+"----------------------Группы в которых нет участников--------------------------------" | Out-File -FilePath $mainCount -Append
+"Группы в которых нет участников="+$nullGroupCount | Out-File -FilePath $mainCount -Append
+
+
+#Get-GPO -All | Measure-Object | Select-Object -ExpandProperty Count
+
+#список пользователей Domain Admins
+$exportPathDA= "$mainPath+DomainAdmins.txt"
+$DomainAdmins = Get-ADGroupMember -Identity "Domain Admins" | Get-ADUser -Properties Name,Enabled,DisplayName,PasswordNeverExpires,PasswordLastSet,whenCreated
+$DomainAdmins | Export-Csv -Path $exportPathDA -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+#список пользователей Schema Admins
+$exportPathDA=$mainPath+"SchemaAdmins.txt"
+$SchemaAdmins = Get-ADGroupMember -Identity "Schema Admins" | Get-ADUser -Properties Name,Enabled,DisplayName,PasswordNeverExpires,PasswordLastSet,whenCreated
+$SchemaAdmins | Export-Csv -Path $exportPathDA -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+#список пользователей Enterprise Admins
+$exportPathDA=$mainPath+"EnterpriceAdmins.txt"
+$EnterpriseAdmins = Get-ADGroupMember -Identity "Enterprise Admins" | Get-ADUser -Properties Name,Enabled,DisplayName,PasswordNeverExpires,PasswordLastSet,whenCreated
+$EnterpriseAdmins | Export-Csv -Path $exportPathDA -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+#Получить список контроллеров домена
+$exportPathDC=$mainPath+"DomainController.txt"
+$DomainController = Get-ADDomainController -Filter * | Select-Object HostName,IPv4Address,OperatingSystem,Enabled,Domain,Forest
+$DomainController | Export-Csv -Path $exportPathDC -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+#Получить список сайтов
+#Get-ADSite
+
+#список рабочих станций
+$exportPathWA=$mainPath+"WorkStationActive.txt"
+$WorkstationActive = (Get-ADComputer -Filter { OperatingSystem -notlike "*Server*" -and Enabled -eq $true } | Measure-Object).Count
+$WorkstationActive | Export-Csv -Path $exportPathDA -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+#список серверов
+$exportPathSA=$mainPath+"DesktopServersActive.txt"
+$ServersActive = (Get-ADComputer -Filter { OperatingSystem -like "*Server*" -and Enabled -eq $true} | Measure-Object).Count
+$ServersActive | Export-Csv -Path $exportPathDA -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+#список рабочих станций
+$exportPathWAList=$mainPath+"WorkStationActiveList.txt"
+$WorkstationActiveList = (Get-ADComputer -Filter { OperatingSystem -notlike "*Server*" -and Enabled -eq $true } -Properties * | Select-Object DNSHostName,SamAccountName,IPv4Address,OperatingSystem,Enabled,Domain,whenCreated,Description,objectSid,LockedOut)
+$WorkstationActiveList | Export-Csv -Path $exportPathWAList -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+#список серверов
+$exportPathSAList=$mainPath+"ServersActiveList.txt"
+$ServersActiveList = (Get-ADComputer -Filter { OperatingSystem  -like "*Server*" -and Enabled -eq $true} -Properties * | Select-Object DNSHostName,SamAccountName,IPv4Address,OperatingSystem,Enabled,Domain,whenCreated,Description,objectSid,LockedOut)
+$ServersActiveList | Export-Csv -Path $exportPathSAList -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+
+#группы в которых нет описания
+$GroupDescNull = (Get-ADGroup -Filter * -Properties description |where {-not $_.Description}| Measure-Object).Count
+
+Write-Host "----------------------Группы в которых нет описания--------------------------------"
+"----------------------Группы в которых нет описания--------------------------------"| Out-File -FilePath $mainCount -Append
+Write-Host "которых нет описания = $GroupDescNull"
+Write-Host "-----------------------------------------------------------------------"
+"Группы в которых нет описания:"+$GroupDescNull| Out-File -FilePath $mainCount -Append
+#групп у которых нет описания
+$exportPathDN=$mainPath+"GroupDescNullList.txt"
+$GroupDescNullList=Get-ADGroup -Filter * -Properties description |where {-not $_.Description}
+$GroupDescNullList | Export-Csv -Path $exportPathDN -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+# Получение информации о сайтах репликации
+Write-Host "-----------------------Сайты репликации------------------------------"
+$exportPathReplicationSites=$mainPath+"ReplicationSites.txt"
+$ReplicationSites = Get-ADReplicationSite -Filter * | Select Name
+$ReplicationSites | Export-Csv -Path $exportPathReplicationSites -Delimiter ';' -NoTypeInformation -Encoding utf8
+Write-Host "Сайты репликации собраны"
+
+
+# Получение информации о подсетях ad
+$exportPathReplicationSubnets=$mainPath+"ReplicationSubnets.txt"
+$ReplicationSubnets = Get-ADReplicationSubnet -Filter * | Select-Object Name, Site
+$ReplicationSubnets | Export-Csv -Path $exportPathReplicationSubnets -Delimiter ';' -NoTypeInformation -Encoding utf8
+
+
+# Получение информации о ссылках репликации
+Write-Host "-----------------------Ссылки репликации------------------------------"
+# Путь к файлу для сохранения данных
+$exportPathReplicationConnections = $mainPath + "ReplicationConnections.txt"
+
+# Получение информации о соединениях репликации
+$ReplicationConnections = Get-ADReplicationConnection -Filter * | 
+    Select-Object Name, ReplicateFromDirectoryServer, ReplicateToDirectoryServer, AutoGenerated, InterSiteTransportProtocol, 
+                  @{Name='ReplicatedNamingContexts';Expression={[string]::Join(";", $_.ReplicatedNamingContexts)}}, 
+                  @{Name='ReplicationSchedule';Expression={if ($_.ReplicationSchedule) { $_.ReplicationSchedule.ToString() } else { "Not Scheduled" }}}
+
+# Запись данных в структурированном виде в текстовый файл
+$ReplicationConnections | ForEach-Object {
+    $output = @"
+--------------------------
+Имя соединения репликации: $($_.DistinguishedName)
+Сервер, с которого происходит репликация: $($_.ReplicateFromDirectoryServer)
+Сервер, на который реплицируются данные: $($_.ReplicateToDirectoryServer)
+Соединение сгенерировано автоматически: $($_.AutoGenerated)
+"@
+    $output | Out-File -FilePath $exportPathReplicationConnections -Append -Encoding utf8
+}
+
+Write-Host "Информация о соединениях репликации собрана в $exportPathReplicationConnections"
+
